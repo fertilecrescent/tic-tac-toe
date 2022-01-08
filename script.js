2ba2fb5d2f4e1599904b229fdaaef493f382fe84
glimmerray <bhodges7@student.gsu.edu>
2022-01-08 18:48:25 -0500

    can add x's and o'x

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("hello world")
+
+var all_squares = document.getElementsByClassName('square')
+var x_turn = true
+
+function squareIsFilled(target) {
+    return target.innerHTML === 'X' | target.innerHTML === 'O'
+}
+
+function whenSquareIsClicked(e) {
+    if (squareIsFilled(e.target)) {
+        return
+    } else {
+        if (x_turn) {
+            e.target.innerHTML = 'X'
+        } else {
+            e.target.innerHTML = 'O'
+        }
+        x_turn = !x_turn
+    }
+}
+
+for (var square of all_squares) {
+    square.addEventListener('click', whenSquareIsClicked)
+}
